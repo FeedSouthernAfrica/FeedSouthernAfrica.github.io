8810d8c8e4af3546032036a9e50de5b857a8d650
Haythem Sahbani <sahbani.haythem@gmail.com>
2015-09-09 18:41:08 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,14 @@
+/* Facebook like page*/
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.4&appId=1534300023482847";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
 /* Navigation bar */
 
 document.getElementById("nav").innerHTML =
@@ -28,15 +39,6 @@ document.getElementById("nav").innerHTML =
     '</div>'
 ;
 
-/* Facebook like page*/
-
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.4&appId=1534300023482847";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
 
 /* Footer */
