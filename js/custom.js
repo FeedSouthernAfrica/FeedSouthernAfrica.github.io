2eef9a7d9eaee96fe1012c1a751cf80dffc2953e
Haythem Sahbani <sahbani.haythem@gmail.com>
2015-09-09 17:31:37 +0200

    Update custom.js
    Changed sheep image size and the list class to leading

--- js/custom.js
+++ js/custom.js
@@ -1,43 +1,104 @@
-/* Navigation bar */
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua--compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="program description - Feed southern Africa" />
+    <meta name="author" content="Haythem Sahbani" />
+    <meta name="keywords" content="volunteer, South Africa, Charity, Children, program description, animal, farm" />
+    <title>Program description - Feed southern Africa</title>
 
-document.getElementById("nav").innerHTML =
-    '<div class="container">'+
-    '<!-- Brand and toggle get grouped for better mobile display -->'+
-    '<div class="navbar-header">'+
-    '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">'+
-    '<span class="sr-only">Toggle navigation</span>'+
-    '<span class="icon-bar"></span>'+
-    '<span class="icon-bar"></span>'+
-    '<span class="icon-bar"></span>'+
-    '</button>'+
-    '<!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->'+
-    '<a class="navbar-brand" href="index.html">Feed Southern Africa</a>'+
-    '</div>'+
-    '<!-- Collect the nav links, forms, and other content for toggling -->'+
-    '<div class="collapse navbar-collapse" >'+
-    '<ul class="nav navbar-nav navbar-right">'+
-    '<li><a href="/">HOME</a></li>'+
-    '<li><a href="/about">ABOUT</a></li>'+
-    '<li><a href="/program_description">THE PROGRAM</a></li>'+
-    '<li><a href="/join">JOIN</a></li>'+
-    '<li><a href="/rates">RATES</a></li>'+
-    '<li><a href="/contact">CONTACT</a></li>'+
-    '</ul>'+
-    '</div>'+
-    '<!-- /.navbar-collapse -->'+
-    '</div>'
-;
-
-/* Footer */
+    <!-- Compiled and minified  bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- CUSTOM STYLE CSS -->
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <!-- GOOGLE FONT -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 
 
 
-document.getElementById("footer").innerHTML = '<div class="container">'+
-    '<div class="row">'+
-'<div class="col-lg-12 text-center">'+
-'<p style="text-align: center"> ' +
-    '&copy;  ' + new Date().getFullYear() + ' Feed Southern Africa. All rights reserved.' +
-    '</p>'+
-'</div>'+
-'</div>'+
-'</div>';
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="brand">The program</div>
+
+    <!-- Navigation Loaded from custom.js-->
+    <nav class="navbar navbar-default " role="navigation" id="nav"></nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr class="hr-400" />
+                    <h2 class="intro-text text-center">The
+                        <strong>Program</strong>
+                    </h2>
+                    <hr class="hr-400" />
+                </div>
+                <div class="col-lg-12 text-center">
+                    <img class="img-responsive img-border " src="img/prog-01.jpg" alt="3 little sheeps" style="width: 80%">
+                    <h2>A Day in the life of our Volunteers
+                        <br>
+                        <small>(Monday to Friday – weekends are free or you can take part in our weekend trips)</small>
+                    </h2>
+                    <dl class="dl-horizontal text-left leading" >
+                        <dt>07:00 – 07:30</dt>
+                            <dd>Early Bird ( Surprise) 2x per week</dd>
+                        <dt>07:30 – 08:00</dt>
+                            <dd>Coffee and Tee</dd>
+                        <dt>08:00 – 09:00</dt>
+                            <dd>
+                                Start preparation for days work.
+                                Getting all equipment together make sure to understand what is to be done for the day.
+                                Divide the work.</dd>
+                        <dt>09:00 - 09:30</dt>
+                            <dd>Breakfast</dd>
+                        <dt>09:30 – 12:30</dt>
+                            <dd>
+                                Work starts.
+                                The volunteers will be divide into 4 Groups and each group has got his/her task that will be given
+                                and explained at the Early Bird or the day before.
+                            </dd>
+                        <dt>12:30 – 14:00</dt>
+                            <dd>Lunch</dd>
+                        <dt>14:00 – 16:00</dt>
+                            <dd>Work will be exceeded and Preparations for the next day will be done</dd>
+                        <dt>16:00 – 16:30</dt>
+                            <dd>Cleaning up and putting everything in place.</dd>
+                    </dl>
+
+                    Free time (swimming, walking, mountain climbing, Horse riding, playing with the Animals, relax)
+                    <hr class="hr-400" />
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- footer loaded from custom.js -->
+    <footer id="footer"></footer>
+
+    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript Bootstrap Core -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" crossorigin="anonymous"></script>
+
+
+    <!-- CUSTOM SCRIPTS  -->
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
